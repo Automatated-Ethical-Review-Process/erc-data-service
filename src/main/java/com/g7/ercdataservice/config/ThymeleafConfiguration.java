70679803cf8b7d3d0246057f138d75a19234e532
Sandaruwan Lakshitha <sandaru.laka@gmail.com>
2022-06-15 00:14:07 +0530

    implement ThymeleafConfiguration.java for email templates

---
+++ src/main/java/com/g7/ercdataservice/config/ThymeleafConfiguration.java
@@ -0,0 +1,28 @@
+package com.g7.ercdataservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+
+@Configuration
+public class ThymeleafConfiguration {
+    public ClassLoaderTemplateResolver emailTemplateResolver() {
+        ClassLoaderTemplateResolver htmlTemplateResolver = new ClassLoaderTemplateResolver();
+        htmlTemplateResolver.setPrefix("templates/");
+        htmlTemplateResolver.setSuffix(".html");
+        htmlTemplateResolver.setTemplateMode("HTML");
+        htmlTemplateResolver.setCharacterEncoding("UTF-8");
+        htmlTemplateResolver.setOrder(1);
+        return htmlTemplateResolver;
+    }
+
+    @Bean
+    @Primary
+    public TemplateEngine templateEngine(){
+        TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.addTemplateResolver(emailTemplateResolver());
+        return templateEngine;
+    }
+}
