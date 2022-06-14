7db80a3d14befa6cb49cdda535055d1e72cbaee1
Sandaruwan Lakshitha <sandaru.laka@gmail.com>
2022-06-14 17:55:59 +0530

    Initial commit

---
+++ src/main/java/com/g7/ercdataservice/ErcDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.g7.ercdataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErcDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErcDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/g7/ercdataservice/ErcDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.g7.ercdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErcDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
